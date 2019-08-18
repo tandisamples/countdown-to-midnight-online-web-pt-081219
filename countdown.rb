7b75712060628605059d6b4d8dedde7083e6b0e0
Tandi Samples <tandisamples@gmail.com>
2019-08-18 20:52:04 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+def countdown(i)
+  while i > 0
+  puts "#{i} SECOND(S)!"
+  i -= 1
+  end
+  "HAPPY NEW YEAR!"
 
-def countdown
 end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
